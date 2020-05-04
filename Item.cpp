f8a1b0ce6e6109f0b0d6fd3b3ad6fc22abd1e0fe
Jon Bos <bos.jon@gmail.com>
2020-05-04 15:50:44 -0500

    test: item class passing object creation

--- Item.cpp
+++ Item.cpp
@@ -1,1 +1,32 @@
 #include "Item.hpp"
+
+Item::Item()
+{
+    this->sku = "";
+    this->price = 0;
+}
+Item::Item(std::string sku, double price)
+{
+    this->sku = sku;
+    this->price = price;
+}
+std::string Item::getSku()
+{
+    return this->sku;
+}
+void Item::setSku(std::string)
+{
+    this->sku = sku;
+}
+double Item::getPrice()
+{
+    return this->price;
+}
+void Item::setPrice(double)
+{
+    this->price = price;
+}
+
+Item::~Item()
+{
+}
--- Item.hpp
+++ Item.hpp
@@ -1,4 +1,21 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
 
+#include <string>
+class Item
+{
+private:
+    std::string sku;
+    double price;
+
+public:
+    Item();
+    Item(std::string, double);
+    ~Item();
+    std::string getSku();
+    void setSku(std::string);
+    double getPrice();
+    void setPrice(double);
+};
+
 #endif // ITEM_HPP
