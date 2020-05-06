ed16320d51f376eb3fb2a8e7bd12c710084bb875
Jon Bos <bos.jon@gmail.com>
2020-05-05 19:31:46 -0500

    Adds integration test

---
+++ test/TransactionIntegrationTests.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "../Transaction.hpp"
+#include "../MarkdownSpecial.hpp"
+#include "../Item.hpp"
+#include <iostream>
+
+TEST(TransactionTests, TransactionIntegrationTest)
+{
+    unordered_map<string, double> priceDB{
+        {"soup", 1.89}, {"meat", 1.99}, {"bread", 1.30}};
+
+    Transaction *t = new Transaction(&priceDB);
+    Special *md = new Markdown("soup", .4, 2);
+    t->addSpecial(*md);
+    // buying 3 soups. soup has 40 cent markdown, limit 2, so total should be ((1.49*2) + 1.89 + 1.99 + 1.30) = 8.16
+    t->scan("soup");
+    t->scan("soup");
+    t->scan("soup");
+    t->scan("meat");
+    t->scan("bread");
+
+    ASSERT_DOUBLE_EQ(t->calculateTotal(), 8.16);
+}
